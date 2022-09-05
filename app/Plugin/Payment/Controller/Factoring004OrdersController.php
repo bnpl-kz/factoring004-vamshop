2ff71d3c4d88adfb95216668ad8d01ea548edd71
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-09-05 17:30:07 +0600

    Adds delivery features

--- app/Plugin/Payment/Controller/Factoring004OrdersController.php
+++ app/Plugin/Payment/Controller/Factoring004OrdersController.php
@@ -1,5 +1,8 @@
 <?php
 
+use BnplPartners\Factoring004\Exception\ErrorResponseException;
+use BnplPartners\Factoring004\Exception\PackageException;
+use BnplPartners\Factoring004VamShop\Handler\OrderStatusHandlerFactory;
 use BnplPartners\Factoring004VamShop\Helper\LoggerFactory;
 use BnplPartners\Factoring004VamShop\Helper\SessionTrait;
 
@@ -29,6 +32,53 @@ class Factoring004OrdersController extends OrdersController
 
     public function admin_new_comment($user = null)
     {
-        parent::admin_new_comment($user);
+        $order = $this->Order->read([], $this->data['Order']['id']);
+
+        if ($order['PaymentMethod']['alias'] !== 'Factoring004') {
+            parent::admin_new_comment($user);
+            return;
+        }
+
+        if ($this->data['Order']['order_status_id'] === $order['Order']['order_status_id']) {
+            parent::admin_new_comment($user);
+            return;
+        }
+
+        try {
+            $handler = OrderStatusHandlerFactory::create($this->data['Order']['order_status_id']);
+
+            if (isset($this->data['__otp_checked'])) {
+                $this->removeSession($handler->getKey() . '_data');
+                parent::admin_new_comment($user);
+                return;
+            }
+
+            $shouldConfirmOtp = $handler->handle($order['Order']);
+
+            if ($shouldConfirmOtp) {
+                $this->putSession($handler->getKey() . '_data', $this->data);
+                $this->redirect('/factoring004-otp/check/' . $handler->getKey());
+                return;
+            }
+
+            parent::admin_new_comment($user);
+        } catch (InvalidArgumentException $e) {
+            parent::admin_new_comment($user);
+        } catch (ErrorResponseException $e) {
+            $response = $e->getErrorResponse();
+            $message = $response->getError() . ': ' . $response->getMessage();
+
+            $this->logger->notice($message, $response->toArray());
+
+            $this->Session->setFlash($message);
+            $this->redirect('/orders/admin_view/' . $order['Order']['id']);
+        } catch (PackageException $e) {
+            $this->logger->error($e);
+
+            $this->Session->setFlash(
+                Configure::read('debug') > 0 ? $e->getMessage() : __d('factoring004', 'An error occurred')
+            );
+            $this->redirect('/orders/admin_view/' . $order['Order']['id']);
+        }
     }
 }
--- app/Plugin/Payment/Controller/Factoring004OtpController.php
+++ app/Plugin/Payment/Controller/Factoring004OtpController.php
@@ -1,9 +1,14 @@
 <?php
 
+use BnplPartners\Factoring004\Exception\ErrorResponseException;
+use BnplPartners\Factoring004\Exception\PackageException;
 use BnplPartners\Factoring004VamShop\Helper\LoggerFactory;
 use BnplPartners\Factoring004VamShop\Helper\SessionTrait;
+use BnplPartners\Factoring004VamShop\Otp\OtpCheckerFactory;
+use BnplPartners\Factoring004VamShop\Request;
 
 App::uses('AppController', 'Controller');
+App::uses('Dispatcher', 'Routing');
 
 require_once ROOT . '/app/Vendor/BnplPartnersFactoring004VamShop/vendor/autoload.php';
 
@@ -13,6 +18,14 @@ require_once ROOT . '/app/Vendor/BnplPartnersFactoring004VamShop/vendor/autoload
 class Factoring004OtpController extends AppController
 {
     use SessionTrait;
+
+    const OTP_CHECK_PATH = '/factoring004-otp/check/';
+    const FORWARD_PATH = '/orders/admin_new_comment/';
+
+    /**
+     * @var string[]
+     */
+    public $uses = ['Order'];
 
     /**
      * @var \Psr\Log\LoggerInterface
@@ -42,6 +55,62 @@ class Factoring004OtpController extends AppController
      */
     public function check($type)
     {
+        $otp = $this->request->data('false.otp');
 
+        if (!is_string($otp) || !preg_match('/^\d{4}$/', $otp)) {
+            $this->Session->setFlash(__d('factoring004', 'Invalid OTP code'));
+            $this->redirect(static::OTP_CHECK_PATH . $type);
+            return;
+        }
+
+        $data = $this->getSession($type . '_data');
+
+        if (!$data) {
+            $this->Session->setFlash(
+                __d('factoring004', 'Session expired. Please back to previous page and update order status again')
+            );
+            $this->redirect(static::OTP_CHECK_PATH . $type);
+            return;
+        }
+
+        $orderId = $data['Order']['id'];
+        $amount = $this->Order->field('total', ['id' => $orderId]);
+
+        try {
+            OtpCheckerFactory::create($type)->check($orderId, $amount, $otp);
+
+            $data['__otp_checked'] = true;
+            $this->forwardToController($data);
+            exit;
+        } catch (ErrorResponseException $e) {
+            $response = $e->getErrorResponse();
+            $message = $response->getError() . ': ' . $response->getMessage();
+
+            $this->logger->notice($message, $response->toArray());
+
+            $this->Session->setFlash($message);
+            $this->redirect(static::OTP_CHECK_PATH . $type);
+        } catch (PackageException $e) {
+            $this->logger->error($e);
+
+            $this->Session->setFlash(
+                Configure::read('debug') > 0 ? $e->getMessage() : __d('factoring004', 'An error occurred')
+            );
+            $this->redirect(static::OTP_CHECK_PATH . $type);
+        }
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     *
+     * @return void
+     */
+    private function forwardToController(array $data)
+    {
+        $request = new Request('POST', static::FORWARD_PATH, false);
+        $request->data = $data;
+
+        $dispatcher = new Dispatcher();
+        $dispatcher->dispatch($request, $this->response);
     }
 }
