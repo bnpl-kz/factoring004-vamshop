f6a407ff9916417dfa91433fcff3bcfa29184fae
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-09-06 15:37:17 +0600

    Overrides order edit controller

--- app/Plugin/Payment/Config/routes.php
+++ app/Plugin/Payment/Config/routes.php
@@ -1,15 +1,16 @@
 <?php
 
 Router::connect('/orders/:action', ['plugin' => 'Payment', 'controller' => 'factoring004Orders']);
+Router::connect('/orders_edit/:action', ['plugin' => 'Payment', 'controller' => 'factoring004OrdersEdit']);
 
 Router::connect(
     '/factoring004-otp/check/:type',
     ['plugin' => 'Payment', 'controller' => 'factoring004Otp', 'action' => 'index', '[method]' => 'GET'],
-    ['pass' => ['type'], 'type' => 'delivery']
+    ['pass' => ['type'], 'type' => 'delivery|full-refund|partial-refund']
 );
 
 Router::connect(
     '/factoring004-otp/check/:type',
     ['plugin' => 'Payment', 'controller' => 'factoring004Otp', 'action' => 'check', '[method]' => 'POST'],
-    ['pass' => ['type'], 'type' => 'delivery']
+    ['pass' => ['type'], 'type' => 'delivery|full-refund|partial-refund']
 );
